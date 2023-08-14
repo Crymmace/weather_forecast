d65a97bfeb71848f9da5ee45c795f5c8f56d2627
Crystal Mace <crymace@outlook.com>
2023-08-14 19:18:25 +0200

    Finish backend functionality and bug fixes.

--- backend.py
+++ backend.py
@@ -7,13 +7,16 @@ load_dotenv()
 api = os.getenv("API")
 
 
-def get_data(place, days=None, kind=None):
+def get_data(place, forecast_days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
 
 
 if __name__ == "__main__":
-    get_data(place="Tokyo")
+    get_data(place="Tokyo", forecast_days=3)
 
